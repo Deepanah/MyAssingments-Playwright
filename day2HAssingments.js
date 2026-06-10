87286e017ce1782df1f29c9f3ff83ea7f82b1bb5
Deepanah <deepanahari14@gmail.com>
2026-06-10 09:47:04 +0530

    Update day2HAssingments.js

--- day2HAssingments.js
+++ day2HAssingments.js
@@ -6,14 +6,8 @@ let contactNumber = 9841726586
 let knowsAutomation = true
 let usesPlaywright
 
-console.log("Username: " + userName)
-console.log("Organization: " + organization)
-console.log("Contact Number: " + contactNumber)
-console.log("Automation knowleadge: " + knowsAutomation)
-console.log("Worked on Playwright? " + usesPlaywright)
-
-console.log("The datatype of Username is: " + typeof userName)
-console.log("Organization datatype is: " + typeof organization)
-console.log("Contact Number is declared as: " + typeof contactNumber)
-console.log("Datatype of automation knowleadge is: " + typeof knowsAutomation)
-console.log("Used playwrigth datatype is: " + typeof usesPlaywright)
+console.log("Username: " + userName, "| Datatype: "  + typeof userName)
+console.log("Organization: " + organization, "| Datatype: "  + typeof organization)
+console.log("Contact Number: " + contactNumber, "| Datatype: " + typeof contactNumber)
+console.log("Automation knowleadge: " + knowsAutomation, "| Datatype: " + typeof knowsAutomation )
+console.log("Worked on Playwright? " + usesPlaywright, "| Datatype: " + typeof usesPlaywright)
