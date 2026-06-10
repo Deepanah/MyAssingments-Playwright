18a92a24d587d1bad6e956c472e6ea55ac57e715
Deepanah <deepanahari14@gmail.com>
2026-06-10 09:37:44 +0530

    Day2 home assingments

---
+++ day2HAssingments.js
@@ -0,0 +1,19 @@
+const { log } = require("node:console")
+
+let userName = "Deepana"
+let organization = "TestLeaf"
+let contactNumber = 9841726586
+let knowsAutomation = true
+let usesPlaywright
+
+console.log("Username: " + userName)
+console.log("Organization: " + organization)
+console.log("Contact Number: " + contactNumber)
+console.log("Automation knowleadge: " + knowsAutomation)
+console.log("Worked on Playwright? " + usesPlaywright)
+
+console.log("The datatype of Username is: " + typeof userName)
+console.log("Organization datatype is: " + typeof organization)
+console.log("Contact Number is declared as: " + typeof contactNumber)
+console.log("Datatype of automation knowleadge is: " + typeof knowsAutomation)
+console.log("Used playwrigth datatype is: " + typeof usesPlaywright)
